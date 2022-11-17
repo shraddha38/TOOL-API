93e87d4366f98fa39a67ff2e6e2e7b739547460d
shraddha38 <shraddhasharmapro@gmail.com>
2022-11-16 19:22:54 -0600

    feat: connect to mongo cloud

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const URL = "mongodb+srv://sraddha:sraddha#123@cluster0.lf2j2dm.mongodb.net/?retryWrites=true&w=majority"
 const app = express();
+
+mongoose.connect(URL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }).then((connection) => {
+    console.log("connected to the database");
+    console.log("connection", connection.connections);
+});
 
 app.get('/', function (req, res) {
     res.send('Hello World');
